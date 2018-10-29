44c6f2202556bf9b7ee4d944617e7e0a70d45198
SuperStarCruise <reachmeabhinay@gmail.com>
2018-10-29 01:52:02 -0400

    first commit :wq

---
+++ tomcruise/src/main/java/superstar/tomcruise/App.java
@@ -0,0 +1,13 @@
+package superstar.tomcruise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
